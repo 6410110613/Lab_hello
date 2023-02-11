d6bc0afeffd11fa807f6177396df3738232f2234
Aeknaronk Thanomchon <6410110613@psu.ac.th>
2023-02-11 11:07:40 +0700

    hello

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("hello, Aeknaronk_Thanomchon\n");
-    printf("6410110613\n");
-
-    return 0;
-}
---
+++ hello.py
@@ -0,0 +1,5 @@
+def hello_me():
+    print("Hi, Aeknaronk Thanomchon")
+    print("6410110613")
+   
+hello_me()
